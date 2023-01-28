66a0d07c479bd0c00d9ca6398310a9a3b4b39703
senatrius <g.makutenas@gmail.com>
2023-01-28 20:42:02 +0200

    validation fixes

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -29,7 +29,7 @@ export const Lightbox = ({
               <path
                 d='m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z'
                 fill='currentColor'
-                fill-rule='evenodd'
+                fillRule='evenodd'
               />
             </svg>
           </button>
@@ -45,9 +45,9 @@ export const Lightbox = ({
                 <path
                   d='M11 1 3 9l8 8'
                   stroke='currentColor'
-                  stroke-width='3'
+                  strokeWidth='3'
                   fill='none'
-                  fill-rule='evenodd'
+                  fillRule='evenodd'
                 />
               </svg>
             </button>
@@ -67,9 +67,9 @@ export const Lightbox = ({
                 <path
                   d='m2 1 8 8-8 8'
                   stroke='currentColor'
-                  stroke-width='3'
+                  strokeWidth='3'
                   fill='none'
-                  fill-rule='evenodd'
+                  fillRule='evenodd'
                 />
               </svg>
             </button>
